2eaff122b8b71ae4cf595931b1affd5497073c0c
Eric Jiang <ejiang@amazon.com>
2020-04-12 16:59:37 -0700

    Remove bullets from player list.

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './PlayerList.css'
 
 export default class PlayerList extends React.Component {
   render() {
@@ -7,7 +8,7 @@ export default class PlayerList extends React.Component {
     return (
       <div>
         <p>{countOnline} player(s) online</p>
-        <ul>
+        <ul className="PlayerList">
         {sample.sort((a, b) => a.name.localeCompare(b.name)).map(p =>
           <li key={p.id}>{p.name}</li>
         )}

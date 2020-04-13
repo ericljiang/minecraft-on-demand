2e9c675cfcbbbcfe0c91c99a5e5d712ed35f36fc
Eric Jiang <ejiang@amazon.com>
2020-04-12 23:55:20 -0700

    Include player skin helm.

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -11,7 +11,7 @@ export default class PlayerList extends React.Component {
         <ul className="PlayerList">
         {sample.sort((a, b) => a.name.localeCompare(b.name)).map(p =>
           <li className="Player" key={p.id}>
-            <img src={`https://minotar.net/avatar/${p.id}/8`} alt={p.name} title={p.name}/>
+            <img src={`https://minotar.net/helm/${p.id}/8`} alt={p.name} title={p.name}/>
           </li>
         )}
         </ul>
